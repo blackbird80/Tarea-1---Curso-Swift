2126fbc867596689b193788cddb5123eefbd03a3
blackbird80 <carloscm80@gmail.com>
2015-10-21 11:19:32 -0500

    Update Contents.swift

--- Tarea1.playground/Contents.swift
+++ Tarea1.playground/Contents.swift
@@ -6,18 +6,22 @@ var numeros = 0...100
 var div = 0
 
 for i in numeros{
-    div = i % 5
-    if div == 0{
-        print("#\(i)\t Bingo!!!")
-    }
-    div = i % 2
-    if div == 0{
-        print("#\(i)\t Par!!!")
-    }else{
-        print("#\(i)\t Impar!!!")
-    }
-    if i >= 30 && i <= 40{
-        print("#\(i)\t Viva Swift!!!")
-    }
+    if i == 0{
+        print("#\(i)\t Caso no Definido")
+        }else{
+            div = i % 5
+            if div == 0{
+                print("#\(i)\t Bingo!!!")
+            }
+            div = i % 2
+            if div == 0{
+                print("#\(i)\t Par!!!")
+            }else{
+                print("#\(i)\t Impar!!!")
+            }
+            if i >= 30 && i <= 40{
+                print("#\(i)\t Viva Swift!!!")
+            }
+        }
 }
 
